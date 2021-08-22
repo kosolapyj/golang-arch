713130967e39183394c0b1ce6b0c420319e4499f
Mike Drits <michael.drits@adp.com>
2021-08-22 19:14:24 -0400

    added encoder

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -43,7 +45,14 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		First: "Jenny",
+	}
 
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("encoded bad data")
+	}
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
