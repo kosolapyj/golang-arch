bdaa71b38de7646fb9297e8b56a6244a6d83b299
Mike Drits <michael.drits@adp.com>
2021-08-24 00:05:45 -0400

    Additional play with base64 encoding

--- main.go
+++ main.go
@@ -6,5 +6,8 @@ import (
 )
 
 func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("username:password")))
+	// user_password := "username:password"
+	user_password := "abcabcabc" // Each 3 characters -> 4 characters
+	fmt.Println(user_password)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte(user_password)))
 }
